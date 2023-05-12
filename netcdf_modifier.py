948f63cde61079ddbe657e39f931141e24ee081d
aliakherati <26212821+aliakherati@users.noreply.github.com>
2023-05-12 15:36:28 -0700

    Update netcdf_modifier.py
    Add 3d met

--- netcdf_modifier.py
+++ netcdf_modifier.py
@@ -264,7 +264,7 @@ class netcdf_modifier:
                     for t in range(new_ds[variable].shape[0]):
                         new_ds[variable][t,z,:,:] = mean_ds[variable][t]
 
-        for var in ['snowewd', 'snowage', ]:
+        for var in ['snowewd', 'snowage', 'tcloudod', 'preciprate', 'cloudtop']:
             new_ds[var][:] = 0.0
 
         for var in ['pblwrf', 'pblcmaq', 'pblysu']:
@@ -279,6 +279,93 @@ class netcdf_modifier:
             if variable not in excluded_variable:
                 df[variable] = mean_ds[variable][:,0]
         excel_mean['all'] = df
+
+        # editing the attributes
+        new_ds.attrs["NCOLS"] = ColumnEnd-ColumnStart
+        new_ds.attrs["NROWS"] = RowEnd-RowStart
+        new_ds.attrs["NLAYS"] = LayerEnd-LayerStart
+
+    return new_ds, excel_mean
+
+    def modify_met_3d(
+        self,
+        Dataset2D,
+        FileName:str,
+        RowStart:int,
+        RowEnd:int,
+        ColumnStart:int,
+        ColumnEnd:int,
+        LayerStart:int,
+        LayerEnd:int,
+        RowIndexAvg:list,
+        ColumnIndexAvg:list,
+        LayerIndexAvg:list,
+    ):
+        # ----------------------------------------------------------------------
+        # Error checking
+        if (len(RowIndexAvg)!=2):
+            raise ValueError("RowIndexAvg must have a length of two.")
+        if (len(ColumnIndexAvg)!=2):
+            raise ValueError("ColumnIndexAvg must have a length of two.")
+        if (len(LayerIndexAvg)!=2):
+            raise ValueError("LayerIndexAvg must have a length of two.")
+        # ---
+        if (ColumnEnd<=ColumnStart):
+            raise ValueError("ColumnStart is bigger than or equal to ColumnEnd")
+        if (RowEnd<RowStart):
+            raise ValueError("RowStart is bigger than or equal to RowEnd")
+        if (LayerEnd<LayerStart):
+            raise ValueError("LayerStart is bigger than or equal to LayerEnd")
+        # -----------------------------------------------------------------------
+
+        # read the file
+        ds = xr.open_dataset(f"{self.directory}/{FileName}")
+
+        # select the window you want to take a mean
+        selected_ds = ds.isel(
+            COL=slice(ColumnIndexAvg[0], ColumnIndexAvg[1]),
+            ROW=slice(RowIndexAvg[0], RowIndexAvg[1]),
+        )
+        # take the mean
+        mean_ds = selected_ds.mean(dim=["ROW", "COL"])
+
+        # select the window you need for your simulation
+        new_ds = ds.isel(
+            COL=slice(ColumnStart, ColumnEnd),
+            ROW=slice(RowStart, RowEnd),
+        )
+
+        # replace the values with the average value for each variable at the surface
+        excluded_variable = ["X", "Y", "layer", "TFLAG", "ETFLAG", "topo", "z", "longitude", "latitude"]
+        for i, variable in enumerate(new_ds.variables):
+            if variable not in excluded_variable:
+                for z in range(new_ds.dims["LAY"]):
+                    for t in range(new_ds[variable].shape[0]):
+                        new_ds[variable][t,z,:,:] = mean_ds[variable][t,z]
+
+        for var in ['cloudwater', 'rainwater', 'grplwater', 'cloudod']:
+            new_ds[var][:] = 0.0
+
+        # set the height of second layer to 3000 m
+        new_ds.variables['z'].values[:,1,:,:] = 3000.
+        # set the height of first layer to pbl height from 2D netcdf file
+        new_ds.variables['z'].values[:,0,:,:] = Dataset2D.variables['pblwrf'].values[:,0,:,:]
+        # set u wind speed to zero 
+        new_ds.variables['uwind'].values[:] = 0
+        # set v wind speed to zero 
+        new_ds.variables['vwind'].values[:] = 0.0926 # m/s (purging layer 2 with 12 hr lifetime)
+
+        # to create excel file
+        excel_mean = {}
+        for i, variable in enumerate(mean_ds.variables):
+            if variable not in excluded_variable:
+                df = pd.DataFrame()
+                df['hour\layer'] = [i for i in range(25)]
+                for z in mean_ds["layer"].values:
+                    z = int(z)
+                    df[z] = mean_ds[variable].values[:,z-1]
+                df["averaged"] = np.nanmean(mean_ds[variable][:].values, axis=1).tolist()
+                excel_mean[variable] = df
 
         # editing the attributes
         new_ds.attrs["NCOLS"] = ColumnEnd-ColumnStart
